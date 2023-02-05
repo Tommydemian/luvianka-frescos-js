cb798b2ea947d43b827698c9ec307e60bf38f185
Tomas <tomas@getcharly.com>
2023-02-05 18:17:39 -0300

    navigation issues

--- src/router/index.js
+++ src/router/index.js
@@ -13,31 +13,26 @@ routes: [
         path: '/',
         name: 'home',
         component: Home,
-        // meta: { requiresAuth: false }
       },
       {
         path: '/productos',
         name: 'products',
         component: () => import('../views/Products.vue'),
-        // meta: { requiresAuth: false }
       },
       {
         path: '/recetas',
         name: 'recipes',
         component: () => import('../views/Recipes.vue'),
-        // meta: { requiresAuth: false }
       },
       {
         path: '/donde-comprar',
         name: 'where-to-buy',
         component: () => import('../views/WhereToBuy.vue'),
-        // meta: { requiresAuth: false }
       },
       {
         path: '/nosotros',
         name: 'who-we-are',
         component: () => import('../views/WhoWeAre.vue')
-        // meta: { requiresAuth: false }
       },
     ]}
 ]
