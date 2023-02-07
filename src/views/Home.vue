fc6b615fb941c2026aa42a6328588e90ba21f832
Tomas <tomas@getcharly.com>
2023-02-06 21:01:52 -0300

    glass effect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -174,6 +174,14 @@ onMounted(() => {
     color: $neutral-clr-800;
     max-width: 70ch;
     transform: translate(-50%, -50%);    
+    background: rgba(245, 245, 245, 0.23);
+border-radius: 16px;
+box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+backdrop-filter: blur(5px);
+-webkit-backdrop-filter: blur(5px);
+border: 1px solid rgba(255, 255, 255, 0.3);
+padding: 1em;
+
     & h3 {
       font-size: $fs-675;
       text-transform: capitalize;
